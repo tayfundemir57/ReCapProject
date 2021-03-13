432b8e0c64e3362d1d937a9a30147cc24ebf42d5
OMEN <OMEN@LAPTOP-DQKA7T18>
2021-03-13 21:54:44 +0300

    Add project files.

---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //Özellik olarak : Id, BrandId, ColorId, ModelYear, DailyPrice, Description alanlarını ekleyiniz. (Brand = Marka)
+
+    public class Car : IEntity
+    {
+        public int CarId { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public string ModelYear { get; set; }
+        public int DailyPrice { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccsess.Concrete.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+
+                Console.WriteLine("Araba Id : " + car.CarId+ "/ "+ "Renk Id : " + car.ColorId + "/ " );
+
+            }
+
+
+        }
+
+    }
+    
+}
