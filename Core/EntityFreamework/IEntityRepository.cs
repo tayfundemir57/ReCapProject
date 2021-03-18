77a7b531facbcfe8ee87cf64fa2e9972dbd3a49c
OMEN <OMEN@LAPTOP-DQKA7T18>
2021-03-18 09:47:17 +0300

    9 Gün Ödevi

---
+++ Core/EntityFreamework/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.EntityFreamework
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new() // T yerine herşey gelebilir..
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null); // Ders:8 Dk:35 ,  filter=null demek filtre vermeyebilirsin demektir.
+
+        T Get(Expression<Func<T, bool>> filter); // filtre vermelisin..
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+    }
+}
