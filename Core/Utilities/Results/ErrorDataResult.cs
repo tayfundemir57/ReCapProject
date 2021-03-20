03609c7b3e110586a5854386f82d374c92a777b6
OMEN <OMEN@LAPTOP-DQKA7T18>
2021-03-21 00:19:45 +0300

    10 Gün Ders Ödev

---
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities
+{
+    public class ErrorDataResult<T> : DataResult<T>
+
+    {
+        public ErrorDataResult(T data, string message) : base(data, false, message) // Versiyon 1
+        {
+
+        }
+
+        public ErrorDataResult(T data) : base(data, false) // Versiyon 2
+        {
+
+        }
+
+        public ErrorDataResult(string message) : base(default, false, message) // Versiyon 3
+        {
+
+        }
+
+        public ErrorDataResult() : base(default, false) // Versiyon 4
+        {
+
+        }
+    }
+}
